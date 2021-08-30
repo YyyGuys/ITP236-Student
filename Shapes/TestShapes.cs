1891ca8ca884fe985822d2e012c03a5438711da3
Bob Dust <bdust@reynolds.edu>
2021-08-30 11:24:46 -0400

    Initial Code from Bob

---
+++ Shapes/TestShapes.cs
@@ -0,0 +1,31 @@
+﻿#define stub
+#undef stub
+using static System.Console;
+
+namespace Shapes
+{
+    public static class TestShapes
+    {
+        public static void Test()
+        {
+#if stub
+            var circle = new Circle(3);
+            ShowShape(circle);
+            var rectangle = new Rectangle(20, 25);
+            ShowShape(rectangle);
+            var square = new Square(18);
+            ShowShape(square);
+            square.Length = 55;
+            ShowShape(square);
+            rectangle = new Rectangle();
+            ShowShape(rectangle);
+#endif
+        }
+#if stub
+        static void ShowShape(Shapes.Shape shape)
+        {
+            WriteLine($"Area = {shape.Area}\t\tPerimeter = {shape.Perimeter}\t\tUom = {shape.UoM}");
+    }
+#endif
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,66 @@
+﻿#define Shapes
+#define EmployeePay
+#define Debugging
+#define StateCollection
+#define Purchasing
+#define EDI
+#define ETL
+//-----------< Comment the #undef for the project you are testing >-----------//
+#undef Shapes
+#undef EmployeePay
+#undef Debugging
+#undef StateCollection
+#undef Purchasing
+#undef EDI
+#undef ETL
+
+using static System.Console;
+
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("<----- Student: YOUR NAME HERE ----->");
+            #region Shapes
+#if Shapes
+            Shapes.TestShapes.Test();
+#endif
+            #endregion
+            #region Employee Pay
+#if EmployeePay
+            EmployeePay.EmployeePay.Test();
+#endif
+            #endregion
+            #region Debugging
+#if Debugging
+            Debugging.Debugging.Test();
+#endif
+            #endregion
+            #region State Collection
+#if StateCollection
+            StateCollection.TestStateCollection.Test();
+#endif
+            #endregion
+            #region Purchasing
+#if Purchasing
+            Purchasing.PurchasingTest.Test();
+            //new Purchasing.TestSales();
+#endif
+            #endregion
+            #region EDI
+#if EDI
+            EDI.TestEDI.Test();
+#endif
+            #endregion
+            #region ETL
+#if ETL
+            ETL.TestEtl.Test();
+#endif
+            #endregion
+            ReadKey();
+        }
+    }
+}
